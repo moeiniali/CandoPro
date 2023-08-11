10e9cf4b42a81060c6929d0bb4dba7c9afa92ca2
Ali Moeini <a.moeini101@gmail.com>
2023-08-11 20:14:40 +0330

    added content

--- jquery/head.js
+++ jquery/head.js
@@ -1,6 +1,22 @@
-$("#toggleBtn").click(function () {
-  $("#toggle").toggle(()=>{
-    this.animate
+$(document).ready(function () {
+  $("#toggleBtn").click(function () {
+    $("#toggle").toggle(()=>{
+      this.getAnimations
+    });
   });
+
+  function handleResize() {
+    if ($(window).width() >= 768) {
+      $('#toggle').hide();
+    }else{
+      null
+    } 
+  }
+
+  // Initial call to handleResize
+  handleResize();
+
+  // Call handleResize on window resize
+  $(window).resize(handleResize);
 
 });
